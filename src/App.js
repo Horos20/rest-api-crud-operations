1e96639f3c73e31891bc73eff5304165cc9a2a74
Matis Rammi <themrrammi@gmail.com>
2022-09-18 23:45:27 +0300

    As a user I can see a list of users [Delivers #183308589]

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  /*    GET initial data on page load    */
+  useEffect(() => {
+    const getData = async () => {
+      return await fetch('https://reqres.in/api/users?page=2', {method: 'GET'}
+    ).then (res => {
+        return res.json()
+    }).then (data => {
+      setData(data)
+    })}
+    getData();
+}, []);
+
   return (
-    <></>
+    <>
+    <table>
+      <thead></thead>
+      <tbody>
+      <tr>
+        <th>Id</th>
+        <th>Email</th>
+        <th>First name</th>
+        <th>Last name</th>
+      </tr>
+      {data.data && data.data.map((row, i) => (
+          <tr key={i}>
+            <td>{row.id}</td>
+            <td>{row.email}</td>
+            <td>{row.first_name}</td>
+            <td>{row.last_name}</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+      </>
   );
 }
 
