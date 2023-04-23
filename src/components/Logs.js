76090aceaf403e8ab5597bd7a3ac9264a14e4ade
Matis Rammi <themrrammi@gmail.com>
2023-04-23 12:13:43 +0300

    Add logs view

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Logs() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const getLogs = async () => {
+      return await fetch("http://localhost:8080/logs", { method: "GET" })
+            .then((res) => {
+              return res.json();
+            })
+            .then((data) => {
+              setData(data)
+            });
+    }
+    getLogs();
+  }, []);
   return (
-    <div>Logs</div>
+    <div>
+      <h1>Logs</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Timestamp</th>
+            <th>Event</th>
+            <th>User</th>
+            <th>Changes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.timestamp}>
+              <td>{item.timestamp}</td>
+              <td>{item.eventName}</td>
+              <td>{item.userName}</td>
+              <td>{item.extraData}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
